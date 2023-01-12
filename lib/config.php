2c8c1904aef33cf2ba84129bf2bcf92b97c1a237
JS Morisset <jsm@surniaulula.com>
2023-01-12 13:20:18 -0800

    v1.0.0

--- lib/config.php
+++ lib/config.php
@@ -17,10 +17,10 @@ if ( ! class_exists( 'WpssoFcfConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssofcf' => array(			// Plugin acronym.
-					'version'     => '1.0.0-dev.4',	// Plugin version.
+					'version'     => '1.0.0',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO FCF',	// Short plugin name.
-					'name'        => 'WPSSO Facebook Catalog Feed XML',
+					'name'        => 'WPSSO Facebook Catalog Feed',
 					'desc'        => 'Facebook Catalog Feed XMLs for WooCommerce, Easy Digital Downloads, and Custom Products.',
 					'slug'        => 'wpsso-facebook-catalog-feed',
 					'base'        => 'wpsso-facebook-catalog-feed/wpsso-facebook-catalog-feed.php',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoFcfConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '14.4.1-dev.4',
+							'min_version'   => '14.4.0',
 						),
 					),
 
--- wpsso-facebook-catalog-feed.php
+++ wpsso-facebook-catalog-feed.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WPSSO Facebook Catalog Feed XML
+ * Plugin Name: WPSSO Facebook Catalog Feed
  * Plugin Slug: wpsso-facebook-catalog-feed
  * Text Domain: wpsso-facebook-catalog-feed
  * Domain Path: /languages
@@ -16,7 +16,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 6.1.1
  * WC Tested Up To: 7.2.3
- * Version: 1.0.0-dev.4
+ * Version: 1.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
