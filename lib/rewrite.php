3131ec748eef24a9739f2652a14e9b56f1aa2bc9
JS Morisset <jsm@surniaulula.com>
2023-01-29 10:25:57 -0800

    v2.0.0-dev.2

--- lib/rewrite.php
+++ lib/rewrite.php
@@ -48,7 +48,7 @@ if ( ! class_exists( 'WpssoCmcfRewrite' ) ) {
 			global $wp_rewrite;
 
 			$rewrite_rules = $wp_rewrite->wp_rewrite_rules();
-			$rewrite_key   = '^(' . WPSSOCMCF_PAGENAME . ')/feed/(rss2)/([^/]+)\.xml$';
+			$rewrite_key   = '^(' . WPSSOCMCF_PAGENAME . ')/feed/(rss2)/([^\./]+)\.xml$';
 			$rewrite_value = 'index.php?pagename=$matches[1]&feed=$matches[2]&locale=$matches[3]';
 
 			if ( empty( $rewrite_rules[ $rewrite_key ] ) || $rewrite_value !== $rewrite_rules[ $rewrite_key ] ) {
