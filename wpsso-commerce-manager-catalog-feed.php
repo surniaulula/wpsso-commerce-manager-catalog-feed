7b09d598b3f827bceada5d4091177335e01bd1d4
JS Morisset <jsm@surniaulula.com>
2023-04-11 09:38:28 -0700

    v2.5.0-dev.3

--- wpsso-commerce-manager-catalog-feed.php
+++ wpsso-commerce-manager-catalog-feed.php
@@ -16,7 +16,7 @@
  * Requires At Least: 5.4
  * Tested Up To: 6.2.0
  * WC Tested Up To: 7.5.1
- * Version: 2.4.0
+ * Version: 2.5.0-dev.3
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
