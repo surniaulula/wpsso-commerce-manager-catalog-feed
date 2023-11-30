9e1ff8239c6f890893d7b3c4b7380cb226a51cf2
JS Morisset <jsm@surniaulula.com>
2023-11-29 17:39:49 -0800

    v4.0.0-b.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -2,7 +2,8 @@
 // 
 // Extract of translatable strings from lib/config.php.
 // 
-_x( 'Facebook Catalog', 'lib file description', 'wpsso-commerce-manager-catalog-feed' );
+_x( 'Meta Catalog', 'lib file description', 'wpsso-commerce-manager-catalog-feed' );
 _x( 'WPSSO Commerce Manager Catalog Feed XML', 'plugin name', 'wpsso-commerce-manager-catalog-feed' );
 _x( 'Facebook and Instagram Commerce Manager Catalog Feed XMLs for WooCommerce and custom product pages.', 'plugin description', 'wpsso-commerce-manager-catalog-feed' );
 _x( 'WPSSO Core', 'plugin name', 'wpsso-commerce-manager-catalog-feed' );
+_x( 'Meta Catalog Feed XML', 'option label', 'wpsso-commerce-manager-catalog-feed' );
--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoCmcfConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssocmcf' => array(			// Plugin acronym.
-					'version'     => '4.0.0-dev.8',	// Plugin version.
+					'version'     => '4.0.0-b.1',	// Plugin version.
 					'opt_version' => '2',		// Increment when changing default option values.
 					'short'       => 'WPSSO CMCF',	// Short plugin name.
 					'name'        => 'WPSSO Commerce Manager Catalog Feed XML',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoCmcfConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '17.3.0-dev.8',
+							'min_version'   => '17.3.0-b.1',
 						),
 					),
 
@@ -60,7 +60,7 @@ if ( ! class_exists( 'WpssoCmcfConfig' ) ) {
 					 */
 					'lib' => array(
 						'submenu' => array(
-							'facebook-catalog' => 'Facebook Catalog',
+							'facebook-catalog' => 'Meta Catalog',
 						),
 					),
 
@@ -149,7 +149,7 @@ if ( ! class_exists( 'WpssoCmcfConfig' ) ) {
 				'cache' => array(
 					'file' => array(
 						'wpssocmcf_feed_' => array(
-							'label'   => 'Commerce Manager Catalog Feed XML',
+							'label'   => 'Meta Catalog Feed XML',
 							'opt_key' => 'cmcf_feed_exp_secs',
 							'filter'  => 'wpsso_cache_expire_cmcf_feed_xml',	// See WpssoUtil->get_cache_exp_secs().
 						),
