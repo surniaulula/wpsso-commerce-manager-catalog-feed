fded8384e01eb8cc5f2a49b456f11a806065c419
JS Morisset <jsm@surniaulula.com>
2023-12-11 08:55:14 -0800

    v4.1.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoCmcfConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssocmcf' => array(			// Plugin acronym.
-					'version'     => '4.1.0-dev.2',	// Plugin version.
+					'version'     => '4.1.0-rc.1',	// Plugin version.
 					'opt_version' => '2',		// Increment when changing default option values.
 					'short'       => 'WPSSO CMCF',	// Short plugin name.
 					'name'        => 'WPSSO Commerce Manager Catalog Feed XML',
