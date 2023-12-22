f9c1db34d1e94fa68ff789c089c691c8f809f6bd
JS Morisset <jsm@surniaulula.com>
2023-12-22 06:39:19 -0800

    v4.3.0

--- lib/xml.php
+++ lib/xml.php
@@ -117,16 +117,9 @@ if ( ! class_exists( 'WpssoCmcfXml' ) ) {
 			$query_args   = array( 'meta_query' => WpssoAbstractWpMeta::get_column_meta_query_og_type( $og_type = 'product', $request_locale ) );
 			$public_ids   = WpssoPost::get_public_ids( $query_args );
 			$rss2_feed    = new Vitalybaev\GoogleMerchant\Feed( $site_title, $site_url, $site_desc, '2.0' );
-			$ship_enabled = false;
-
-			/*
-			 * Add or exclude the addition of shipping information to the Open Graph meta tags.
-			 */
-			add_filter( 'wpsso_og_add_mt_shipping_offers', ( $ship_enabled ? '__return_true' : '__return_false' ), 1000, 1 );
 
 			if ( $wpsso->debug->enabled ) {
 
-				$wpsso->debug->log( 'add shipping is ' . ( $ship_enabled ? 'enabled' : 'disabled' ) );
 				$wpsso->debug->log( 'adding ' . count( $public_ids ) . ' public ids' );
 				$wpsso->debug->log_arr( 'public_ids', $public_ids );
 				$wpsso->debug->mark_diff( 'adding ' . count( $public_ids ) . ' public ids' );
@@ -189,11 +182,6 @@ if ( ! class_exists( 'WpssoCmcfXml' ) ) {
 			}
 
 			unset( $public_ids );
-
-			/*
-			 * Remove the filter hook, just in case.
-			 */
-			remove_filter( 'wpsso_og_add_mt_shipping_offers', ( $ship_enabled ? '__return_true' : '__return_false' ), 1000 );
 
 			if ( $wpsso->debug->enabled ) {
 
