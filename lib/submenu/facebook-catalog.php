3d5125b8384baffe0baa525fdd13b78fef64437c
jsmoriss <jsm@surniaulula.com>
2024-10-31 17:23:55 +0000

    v4.8.0-rc.3

--- lib/submenu/facebook-catalog.php
+++ lib/submenu/facebook-catalog.php
@@ -108,21 +108,26 @@ if ( ! class_exists( 'WpssoCmcfSubmenuFacebookCatalog' ) && class_exists( 'Wpsso
 						$feed_type   = 'feed';
 						$feed_format = $this->p->options[ 'cmcf_' . $feed_type . '_format' ];
 						$url         = WpssoCmcfRewrite::get_url( $locale, $feed_type, $feed_format );
-						$xml         = WpssoCmcfXml::get( $locale, $feed_type, $feed_format );
 						$css_id      = SucomUtil::sanitize_css_id( 'cmcf_feed_xml_' . $locale );
-						$item_count  = substr_count( $xml, 'atom' === $feed_format? '<entry>' : '<item>' );
-						$img_count   = substr_count( $xml, '<g:image_link>' );
-						$addl_count  = substr_count( $xml, '<g:additional_image_link>' );
-						$xml_size    = number_format( ( strlen( $xml ) / 1024 ) );	// XML size in KB.
+						$xml_info    = array();
 
-						unset( $xml );
+						if ( ! SucomUtil::get_const( 'WPSSOCMCF_XML_INFO_DISABLE', false ) ) {
 
-						$xml_info = array(
-							sprintf( _x( '%s feed items', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $item_count ),
-							sprintf( _x( '%s image links', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $img_count ),
-							sprintf( _x( '%s additional image links', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $addl_count ),
-							sprintf( _x( '%s KB file size', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $xml_size ),
-						);
+							$xml         = WpssoCmcfXml::get( $locale, $feed_type, $feed_format );
+							$item_count  = substr_count( $xml, 'atom' === $feed_format? '<entry>' : '<item>' );
+							$img_count   = substr_count( $xml, '<g:image_link>' );
+							$addl_count  = substr_count( $xml, '<g:additional_image_link>' );
+							$xml_size    = number_format( ( strlen( $xml ) / 1024 ) );	// XML size in KB.
+
+							unset( $xml );
+
+							$xml_info = array(
+								sprintf( _x( '%s feed items', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $item_count ),
+								sprintf( _x( '%s image links', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $img_count ),
+								sprintf( _x( '%s additional image links', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $addl_count ),
+								sprintf( _x( '%s KB file size', 'option comment', 'wpsso-commerce-manager-catalog-feed' ), $xml_size ),
+							);
+						}
 
 						$table_rows[ $css_id ] = '' .
 							$this->form->get_th_html( $native_name, $css_class = '', $css_id,
